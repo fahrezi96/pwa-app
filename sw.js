55ffe524eb65ee303dfd4feee47f6cef0217520d
fahrezi96 <ifmuhamad@gmail.com>
2022-07-17 14:47:31 +0700

    up cache v

--- sw.js
+++ sw.js
@@ -1,16 +1,16 @@
-const staticCacheName = 'site-static-v1.1';
-const dynamicCacheName = 'site-dynamic-v1';
+const staticCacheName = "site-static-v1.2";
+const dynamicCacheName = "site-dynamic-v1";
 const assets = [
-  '/pwa-app/',
-  '/pwa-app/index.html',
-  '/pwa-app/assets/js/script.js',
-  '/pwa-app/assets/js/ui.js',
-  '/pwa-app/assets/js/materialize.min.js',
-  '/pwa-app/assets/css/style.css',
-  '/pwa-app/assets/css/materialize.min.css',
-  '/pwa-app/assets/img/icon.png',
-  'https://fonts.googleapis.com/icon?family=Material+Icons',
-  '/pwa-app/pages/fallback.html',
+  "/pwa-app/",
+  "/pwa-app/index.html",
+  "/pwa-app/assets/js/script.js",
+  "/pwa-app/assets/js/ui.js",
+  "/pwa-app/assets/js/materialize.min.js",
+  "/pwa-app/assets/css/style.css",
+  "/pwa-app/assets/css/materialize.min.css",
+  "/pwa-app/assets/img/icon.png",
+  "https://fonts.googleapis.com/icon?family=Material+Icons",
+  "/pwa-app/pages/fallback.html",
 ];
 
 // cache size limit function
@@ -25,28 +25,30 @@ const limitCacheSize = (name, size) => {
 };
 
 // install sw
-self.addEventListener('install', (e) => {
+self.addEventListener("install", (e) => {
   e.waitUntil(
     caches.open(staticCacheName).then((cache) => {
-      console.log('caching all assets');
+      console.log("caching all assets");
       cache.addAll(assets);
     })
   );
 });
 
 // activate event
-self.addEventListener('activate', (e) => {
+self.addEventListener("activate", (e) => {
   e.waitUntil(
     caches.keys().then((keys) => {
       return Promise.all(
-        keys.filter((key) => key !== staticCacheName && key !== dynamicCacheName).map((key) => caches.delete(key))
+        keys
+          .filter((key) => key !== staticCacheName && key !== dynamicCacheName)
+          .map((key) => caches.delete(key))
       );
     })
   );
 });
 
 // fetch event
-self.addEventListener('fetch', (e) => {
+self.addEventListener("fetch", (e) => {
   e.respondWith(
     caches
       .match(e.request)
@@ -63,8 +65,8 @@ self.addEventListener('fetch', (e) => {
         );
       })
       .catch(() => {
-        if (e.request.url.indexOf('.html') > -1) {
-          return caches.match('/pwa-app/pages/fallback.html');
+        if (e.request.url.indexOf(".html") > -1) {
+          return caches.match("/pwa-app/pages/fallback.html");
         }
       })
   );
